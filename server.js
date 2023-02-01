82faca995f9c12ce2b3cc385906bda35aadb55cc
mayank-Pareek <mpareek365@gmail.com>
2023-02-01 17:22:34 -0500

    update declaration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
- *  Name: MAYANK KUMAR      Student ID: 145998217        Date: 02 FEB 2023
+ *  Name: MAYANK KUMAR      Student ID: 145998217        Date: 01 FEB 2023
  *
  *  Cyclic Web App URL: https://grumpy-pear-dove.cyclic.app
  *

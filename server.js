8dda2099c7af3e280e62ebbc5b9c7eb924b2e7e3
mayank-Pareek <mpareek365@gmail.com>
2023-04-13 18:15:23 -0400

    Final commit for AS#6

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
- *  WEB322 – Assignment 05
- *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
+ *  WEB322 – Assignment 06
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
- *  Name: MAYANK KUMAR      Student ID: 145998217        Date: 28 MARCH 2023
+ *  Name: MAYANK KUMAR      Student ID: 145998217        Date: 13 APRIL 2023
  *
  *  Cyclic Web App URL: https://grumpy-pear-dove.cyclic.app
  *
